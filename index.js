ffbe5c3264380f62cd48243747664138aebe85a9
sahil_techmapperz <sahil.laskar@techmapperz.com>
2024-06-05 15:57:21 +0530

    Allows all origins

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ const router = require('./routes/index')
 const cookiesParser = require('cookie-parser')
 const { app, server } = require('./socket/index')
 
+
+const allowedOrigin = process.env.FRONTEND_URL; // Set this in your .env file
 // const app = express()
 app.use(cors({
-    origin : '*',
-    credentials : true
-}))
+    origin: function (origin, callback) {
+        if (!origin || origin === allowedOrigin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the methods you want to allow
+    allowedHeaders: ['Content-Type', 'Authorization'] // Specify the headers you want to allow
+}));
+
+
 app.use(express.json())
 app.use(cookiesParser())
 
@@ -27,6 +39,7 @@ app.use('/api',router)
 
 connectDB().then(()=>{
     console.log("Server connected to the mongoDB server");
+    console.log(allowedOrigin);
     server.listen(PORT,()=>{
         console.log("server running at  " + PORT)
     })
