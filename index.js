d012f5dd3e6c19a4eafe9d5cd7c99f5ff8e06332
sahil_techmapperz <sahil.laskar@techmapperz.com>
2024-06-05 16:19:59 +0530

    Allows to a domain

--- index.js
+++ index.js
@@ -1,45 +1,33 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const connectDB = require('./config/connectDB');
-const router = require('./routes/index');
-const cookiesParser = require('cookie-parser');
-const { app, server } = require('./socket/index');
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const connectDB = require('./config/connectDB')
+const router = require('./routes/index')
+const cookiesParser = require('cookie-parser')
+const { app, server } = require('./socket/index')
 
-const allowedOrigin = process.env.FRONTEND_URL; // Set this in your .env file
-
+// const app = express()
 app.use(cors({
-    origin: function (origin, callback) {
-        console.log(`Request origin: ${origin}`); // Log the origin for debugging
-        if (!origin || origin === allowedOrigin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the methods you want to allow
-    allowedHeaders: ['Content-Type', 'Authorization'] // Specify the headers you want to allow
-}));
+    origin : process.env.FRONTEND_URL,
+    credentials : true
+}))
+app.use(express.json())
+app.use(cookiesParser())
 
-app.use(express.json());
-app.use(cookiesParser());
+const PORT = process.env.PORT || 8080
 
-const PORT = process.env.PORT || 8080;
-
-app.get('/', (request, response) => {
+app.get('/',(request,response)=>{
     response.json({
-        message: "Server running at " + PORT
-    });
-});
+        message : "Server running at " + PORT
+    })
+})
 
-// API endpoints
-app.use('/api', router);
+//api endpoints
+app.use('/api',router)
 
-connectDB().then(() => {
-    console.log("Server connected to the MongoDB server");
-    console.log(`Allowed origin: ${allowedOrigin}`);
-    server.listen(PORT, () => {
-        console.log("Server running at " + PORT);
-    });
-});
+connectDB().then(()=>{
+    console.log("Server connected to the mongoDB server");
+    server.listen(PORT,()=>{
+        console.log("server running at  " + PORT)
+    })
+})
